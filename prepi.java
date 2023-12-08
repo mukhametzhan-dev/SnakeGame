2db50feb074bd179f07394d38f3cb0fd8e399723
Mukhamedzhan <128323894+mukhametzhan-dev@users.noreply.github.com>
2023-12-09 00:24:54 +0600

    Added prepi class
    Препятствие были добавлены

---
+++ prepi.java
@@ -0,0 +1,17 @@
+public class prepi{
+	private final int x;
+        private final int y;
+
+        public prepi(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+}
